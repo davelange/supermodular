af6bc065d801b793ee74e211eb336ef041cd2a38
David Lange <lange@finiam.com>
2025-06-17 11:49:12 +0100

    setup scene

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,108 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from "three";
+import {
+  EffectComposer,
+  OrbitControls,
+  RenderPass,
+  ShaderPass,
+} from "three/examples/jsm/Addons.js";
+import Stats from "three/examples/jsm/libs/stats.module.js";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+class MyScene {
+  scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer();
+  canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
+  camera = new THREE.OrthographicCamera();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  stats = new Stats();
+  controls = new OrbitControls(this.camera, this.canvas);
+
+  settings: Record<string, any> = {};
+  mouse = new THREE.Vector2(0, 0);
+  prevMouse = new THREE.Vector2(0, 0);
+
+  constructor() {
+    this.setupRenderer();
+    this.setupCamera();
+    this.setupResize();
+    this.controls = new OrbitControls(this.camera, this.canvas);
+    this.trackMouse();
+    this.addObjects();
+    this.initSettings();
+  }
+
+  initSettings() {}
+
+  setupRenderer() {
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true,
+      //alpha: true,
+    });
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  }
+
+  setupCamera() {
+    let frustrum = this.height;
+    let aspect = this.width / this.height;
+    this.camera = new THREE.OrthographicCamera(
+      (frustrum * aspect) / -2,
+      (frustrum * aspect) / 2,
+      frustrum / 2,
+      frustrum / -2,
+      -1000,
+      1000
+    );
+    this.camera.position.set(0, 0, 2);
+    this.camera.zoom = 10;
+    this.camera.updateProjectionMatrix();
+  }
+
+  setupResize() {
+    window.addEventListener("resize", () => {
+      // Update sizes
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+
+      // Update camera
+      this.camera.updateProjectionMatrix();
+
+      // Update renderer
+      this.renderer.setSize(this.width, this.height);
+      this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+  }
+
+  trackMouse() {
+    window.addEventListener("mousemove", (event) => {
+      this.mouse.x = event.clientX - this.width / 2;
+      this.mouse.y = this.height / 2 - event.clientY;
+    });
+  }
+
+  addObjects() {
+    const geometry = new THREE.BoxGeometry(10, 10, 10);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const cube = new THREE.Mesh(geometry, material);
+    this.scene.add(cube);
+  }
+
+  clock = new THREE.Clock();
+
+  render() {
+    const elapsedTime = this.clock.getElapsedTime();
+
+    this.controls.update();
+
+    // Render
+    this.renderer.render(this.scene, this.camera);
+
+    window.requestAnimationFrame(this.render.bind(this));
+  }
+}
+
+let myScene = new MyScene();
+myScene.render();
